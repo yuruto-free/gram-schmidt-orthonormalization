ef1e644d10346933171b747f5a50635c62b0c548
yutori-free <mailorder2receiver@gmail.com>
2022-04-14 02:52:46 +0900

    clang-formatの設定ファイルを追加 .gitignoreを更新 clang-formatを用いてコードを整形

--- src/gram_schmidt.c
+++ src/gram_schmidt.c
@@ -1,8 +1,8 @@
-#include <stddef.h>
-#include <stdint.h>
+#include "gram_schmidt.h"
 #include <malloc.h>
 #include <math.h>
-#include "gram_schmidt.h"
+#include <stddef.h>
+#include <stdint.h>
 
 #define RETURN_OK_GS (0)
 #define RETURN_NG_GS (1)
@@ -15,7 +15,7 @@
  * \param[out] norm ノルムの値
  * \retval RETURN_OK_GS 正常終了
  * \retval RETURN_NG_GS 異常終了
-*/
+ */
 static int32_t euclid_norm(int32_t dim, double *vec, double *norm);
 /*
  * \brief 内積の計算
@@ -25,8 +25,9 @@ static int32_t euclid_norm(int32_t dim, double *vec, double *norm);
  * \param[out] *dot       内積値
  * \retval RETURN_OK_GS 正常終了
  * \retval RETURN_NG_GS 異常終了
-*/
-static int32_t inner_product(int32_t dim, double *left_vec, double *right_vec, double *dot);
+ */
+static int32_t inner_product(int32_t dim, double *left_vec, double *right_vec,
+                             double *dot);
 /*
  * \brief ベクトルの差の計算
  * \param[in]    dim     次元数
@@ -35,18 +36,21 @@ static int32_t inner_product(int32_t dim, double *left_vec, double *right_vec, d
  * \param[in]    *base   引く数となるベクトル
  * \retval RETURN_OK_GS 正常終了
  * \retval RETURN_NG_GS 異常終了
-*/
+ */
 static int32_t sub_vector(int32_t dim, double coef, double *target, double *base);
 /*
  * \brief 直交するベクトルの計算
  * \param[in]    dim         次元数
  * \param[in]    current_idx 計算対象のインデックス
  * \param[inout] *dots       内積値（作業用）
- * \param[inout] vecs        ベクトル（dim * vec_num、正規直交化するベクトル：行ベクトル形式）
+ * \param[inout] vecs        ベクトル
+ *                           サイズ：dim * vec_num、
+ *                           正規直交化するベクトル：行ベクトル形式
  * \retval RETURN_OK_GS 正常終了
  * \retval RETURN_NG_GS 異常終了
-*/
-static int32_t orthogonal_vector(int32_t dim, int32_t current_idx, double *dots, double *vecs);
+ */
+static int32_t orthogonal_vector(int32_t dim, int32_t current_idx, double *dots,
+                                 double *vecs);
 /*
  * \brief スケーリング
  * \param[in]    dim  次元数
@@ -54,7 +58,7 @@ static int32_t orthogonal_vector(int32_t dim, int32_t current_idx, double *dots,
  * \param[inout] vec  ベクトル
  * \retval RETURN_OK_GS 正常終了
  * \retval RETURN_NG_GS 異常終了
-*/
+ */
 static int32_t scaling(int32_t dim, double coef, double *vec);
 
 int32_t GS_orthonormalization(int32_t dim, int32_t vec_num, double *vecs) {
@@ -78,7 +82,7 @@ int32_t GS_orthonormalization(int32_t dim, int32_t vec_num, double *vecs) {
             goto EXIT_GS_ORTH;
         }
         /* e_{0}を計算 */
-        func_val = scaling(dim, 1.0/norm, target);
+        func_val = scaling(dim, 1.0 / norm, target);
         if ((int32_t)RETURN_OK_GS != func_val) {
             goto EXIT_GS_ORTH;
         }
@@ -92,11 +96,12 @@ int32_t GS_orthonormalization(int32_t dim, int32_t vec_num, double *vecs) {
             }
             /* f_{k}のノルムを計算 */
             func_val = euclid_norm(dim, target, &norm);
-            if (((int32_t)RETURN_OK_GS != func_val) || (fabs(norm) < (double)MEPS_GS)) {
+            if (((int32_t)RETURN_OK_GS != func_val) ||
+                (fabs(norm) < (double)MEPS_GS)) {
                 goto EXIT_GS_ORTH;
             }
             /* e_{k}を計算 */
-            func_val = scaling(dim, 1.0/norm, target);
+            func_val = scaling(dim, 1.0 / norm, target);
             if ((int32_t)RETURN_OK_GS != func_val) {
                 goto EXIT_GS_ORTH;
             }
@@ -121,13 +126,14 @@ static int32_t euclid_norm(int32_t dim, double *vec, double *norm) {
             sum += vec[i] * vec[i];
         }
         (*norm) = sqrt(sum);
-        ret = (int32_t)RETURN_OK_GS;
+        ret     = (int32_t)RETURN_OK_GS;
     }
 
     return ret;
 }
 
-static int32_t inner_product(int32_t dim, double *left_vec, double *right_vec, double *dot) {
+static int32_t inner_product(int32_t dim, double *left_vec, double *right_vec,
+                             double *dot) {
     int32_t ret = (int32_t)RETURN_NG_GS;
     int32_t i;
     double sum;
@@ -139,7 +145,7 @@ static int32_t inner_product(int32_t dim, double *left_vec, double *right_vec, d
             sum += left_vec[i] * right_vec[i];
         }
         (*dot) = sum;
-        ret = (int32_t)RETURN_OK_GS;
+        ret    = (int32_t)RETURN_OK_GS;
     }
 
     return ret;
@@ -159,7 +165,8 @@ static int32_t sub_vector(int32_t dim, double coef, double *target, double *base
     return ret;
 }
 
-static int32_t orthogonal_vector(int32_t dim, int32_t current_idx, double *dots, double *vecs) {
+static int32_t orthogonal_vector(int32_t dim, int32_t current_idx, double *dots,
+                                 double *vecs) {
     int32_t ret = (int32_t)RETURN_NG_GS;
     int32_t i;
     int32_t func_val;
