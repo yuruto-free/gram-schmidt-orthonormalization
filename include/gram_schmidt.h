119cf25784c4a6d9ff205fa583bb0c92a532da0c
yutori-free <mailorder2receiver@gmail.com>
2022-04-14 20:35:58 +0900

    変数名を更新。 README.mdに必要なコマンド一覧を追加。

--- include/gram_schmidt.h
+++ include/gram_schmidt.h
@@ -8,14 +8,14 @@
 
 /*
  * \brief グラム・シュミットの正規直交化法による正規直交基底への変換
- * \param[in]    dim     次元数
- * \param[in]    vec_num 正規直交化するベクトルの数
- * \param[inout] vecs    ベクトル
- *                       サイズ：dim * vec_num
- *                       正規直交化するベクトル：行ベクトル形式
+ * \param[in]    dim  次元数
+ * \param[in]    num  正規直交化するベクトルの数
+ * \param[inout] vecs ベクトル
+ *                    サイズ：dim * vec_num
+ *                    正規直交化するベクトル：行ベクトル形式
  * \retval       GS_OK   正常終了
  * \retval       GS_NG   異常終了
  */
-int32_t GS_orthonormalization(int32_t dim, int32_t vec_num, double *vecs);
+int32_t GS_orthonormalization(int32_t dim, int32_t num, double *vecs);
 
 #endif
